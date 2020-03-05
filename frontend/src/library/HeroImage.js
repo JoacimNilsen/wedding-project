fbf3fdd4c3ffb8a4dc8d121c0f26c7d77e25d0e7
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-03-05 21:38:20 +0100

    sizing issues fixed

--- frontend/src/library/HeroImage.js
+++ frontend/src/library/HeroImage.js
@@ -3,25 +3,24 @@ import styled from 'styled-components'
 import hero from 'assets/hero.jpg'
 
 
-const Wrapper = styled.div`
-border: 3px solid white;
-border-radius: 50%;
-`
 
 const Hero = styled.div`
 background-image: url(${hero});
 background-position: 50% 20%;
-background-size: 95%
+background-size: cover;
 border: 3px solid white;
 border-radius: 50%;
-width: 500px;
-height: 500px;
+width: 300px;
+height: 300px;
+margin-top: 40px;
+  @media (min-width: 668px) {
+    width: 500px;
+    height: 500px;
+  }
 `
 
 export const HeroImage = () => {
   return (
-    <Wrapper>
     <Hero />
-    </Wrapper>
   )
 }

183a54e19d240da6f5d743f1572f9e08d09e660b
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-03-27 21:05:57 +0100

    Uncommented the presumed problem..

--- backend/server.js
+++ backend/server.js
@@ -202,7 +202,7 @@ app.delete('/guests/:id', async (req, res) => {
 //     //failed
 //     res.status(400).json({ message: "Could not upload image", error: err.errors })
 //   }
-// // Start the server
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`)
-// })
+// Start the server
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`)
+})

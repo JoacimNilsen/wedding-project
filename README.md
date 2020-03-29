9bde1e92b14cbb711a6e99bc17d66b7f8cd9f93e
JoacimNilsen <joacimnilsen1986@gmail.com>
2020-03-29 20:55:47 +0200

    added live API

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-This is my final project for the Technigo bootcamp. 
-The goal was to implement all the technologies we have learned during this 6 month course. The time 
+#Final project
+
+This is my final project for the Technigo bootcamp.
+The goal was to implement all the technologies we have learned during this 6 month course. The time we were expected to spend on the project is 80 hours.
+My friends are getting married so I decided to make a website for their wedding, with various guestlist functions.
+My stretch goal was to make a gallery in wich guests could upload images from the wedding and the party. I did not have time with this but I am currently working on it even to school is finished.
+
+Deployed demo site: https://engerdahl-wedding.netlify.com/
+
+##Technologies used
+
+Frontend:
+
+- Javascript ES6
+- React
+- React Router
+- CSS3
+- Styled components
+- Redux
+- API
+
+Backend:
+
+- Express
+- Mongoose
+- MongoDB
--- frontend/src/reducers/admins.js
+++ frontend/src/reducers/admins.js
@@ -1,52 +1,52 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { ui } from "./ui"
+import { createSlice } from '@reduxjs/toolkit'
+import { ui } from './ui'
 
 export const admins = createSlice({
-  name: "admins",
-  initialState: {
-    accessToken: localStorage.getItem("accessToken"),
-    adminName: localStorage.getItem("adminName")
-  },
-  reducers: {
-    setAdminName: (state, action) => {
-      localStorage.setItem("adminName", action.payload)
-      state.adminName = action.payload
-    },
-    removeAdminName: (state, action) => {
-      state.adminName = localStorage.removeItem("adminName", action.payload)
-    },
-    setAccessToken: (state, action) => {
-      localStorage.setItem("accessToken", action.payload)
-      state.accessToken = action.payload
-    },
-    removeAccessToken: (state, action) => {
-      localStorage.removeItem("accessToken", action.payload)
-      state.accessToken = ""
-    }
-  }
+	name: 'admins',
+	initialState: {
+		accessToken: localStorage.getItem('accessToken'),
+		adminName: localStorage.getItem('adminName')
+	},
+	reducers: {
+		setAdminName: (state, action) => {
+			localStorage.setItem('adminName', action.payload)
+			state.adminName = action.payload
+		},
+		removeAdminName: (state, action) => {
+			state.adminName = localStorage.removeItem('adminName', action.payload)
+		},
+		setAccessToken: (state, action) => {
+			localStorage.setItem('accessToken', action.payload)
+			state.accessToken = action.payload
+		},
+		removeAccessToken: (state, action) => {
+			localStorage.removeItem('accessToken', action.payload)
+			state.accessToken = ''
+		}
+	}
 })
 
 export const fetchAdmin = loginValues => {
-  return dispatch => {
-    dispatch(ui.actions.setLoading(true))
-    fetch(`http://localhost:8080/login`, {
-      method: "POST",
-      body: JSON.stringify(loginValues),
-      headers: { "Content-Type": "application/json" }
-    })
-      .then(res => res.json())
-      .then(json => {
-        if (json.notFound !== true) {
-          dispatch(admins.actions.setAccessToken(json.accessToken))
-          dispatch(admins.actions.setAdminName(json.name))
-          dispatch(ui.actions.setLoading(false))
-          dispatch(ui.actions.setLoginFailed(false))
-          dispatch(ui.actions.setLoginOpen(false))
-        } else {
-          dispatch(ui.actions.setLoading(false))
-          dispatch(ui.actions.setLoginFailed(true))
-        }
-      })
-      .catch(err => console.log("error", err))
-  }
+	return dispatch => {
+		dispatch(ui.actions.setLoading(true))
+		fetch(`https://wedding-project-fixed.herokuapp.com/login`, {
+			method: 'POST',
+			body: JSON.stringify(loginValues),
+			headers: { 'Content-Type': 'application/json' }
+		})
+			.then(res => res.json())
+			.then(json => {
+				if (json.notFound !== true) {
+					dispatch(admins.actions.setAccessToken(json.accessToken))
+					dispatch(admins.actions.setAdminName(json.name))
+					dispatch(ui.actions.setLoading(false))
+					dispatch(ui.actions.setLoginFailed(false))
+					dispatch(ui.actions.setLoginOpen(false))
+				} else {
+					dispatch(ui.actions.setLoading(false))
+					dispatch(ui.actions.setLoginFailed(true))
+				}
+			})
+			.catch(err => console.log('error', err))
+	}
 }
